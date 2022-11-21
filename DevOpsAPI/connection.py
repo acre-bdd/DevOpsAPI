e601cfe382f753820215f90562913b42498700e4
Claudio Klingler <ck@realtime-projects.com>
2022-11-21 20:01:49 +0100

    Refactor connection.

--- DevOpsAPI/connection.py
+++ DevOpsAPI/connection.py
@@ -1,4 +1,6 @@
+import json as js
 import requests
+import logging
 
 
 class Connection:
@@ -9,23 +11,32 @@ class Connection:
         self.apikey = apikey
         self.headers = {'Content-type': 'application/json-patch+json'}
 
+    def get(self, *args, **kwargs):
+        return self.request("get", *args, **kwargs)
+
+    def delete(self, *args, **kwargs):
+        return self.request("delete", *args, **kwargs)
+
+    def post(self, *args, **kwargs):
+        return self.request("post", *args, **kwargs)
+
+    def patch(self, *args, **kwargs):
+        return self.request("patch", *args, **kwargs)
+
+    def request(self, rt, fnc, json=None, is_json=False):
+        headers = self.headers if is_json else None
+        print(f"calling: {rt} {fnc}")
+        response = requests.request(rt, self._uri(fnc), json=json, auth=self._auth, headers=headers)
+        if response.status_code != 200:
+            logging.warning(js.dumps(response.json(), indent=4))
+        response.raise_for_status()
+        return response
+
     @property
-    def auth(self):
+    def _auth(self):
         return (self.user, self.apikey)
 
-    def uri(self, fnc):
+    def _uri(self, fnc):
         return f"https://dev.azure.com/{self.organization}/{self.project}/_apis/{fnc}?api-version=7.0"
 
-    def get(self, fnc, json=None):
-        return requests.get(self.uri(fnc), auth=self.auth, headers=self.headers, json=json)
-
-    def post(self, fnc, json=None, is_json=False):
-        headers = self.headers if is_json else None
-        return requests.post(self.uri(fnc), auth=self.auth, headers=headers, json=json)
-
-    def patch(self, fnc, json=None, is_json=False):
-        headers = self.headers if is_json else None
-        return requests.patch(self.uri(fnc), auth=self.auth, headers=headers, json=json)
-
-    def delete(self, fnc):
-        return requests.delete(self.uri(fnc), auth=self.auth)
+    
