7701f04f745fc86a866a8a6aebc192a55ebee787
Claudio Klingler <ck@realtime-projects.com>
2022-12-13 13:38:12 +0100

    Fix project class.

--- DevOpsAPI/projects.py
+++ DevOpsAPI/projects.py
@@ -6,13 +6,13 @@ import json
 class Project(FunctionClass):
     def __init__(self, _c, json=None):
         _c = Connection(_c.organization, None, _c.user, _c.apikey)
-        super().__init__(self, "projects", _c, json)
+        super().__init__("projects", _c, json)
 
 
 class Projects(FunctionManager):
     def __init__(self, _c):
         _c = Connection(_c.organization, None, _c.user, _c.apikey)
-        super().__init__("projects", _c)
+        super().__init__("projects", _class=Project, _c=_c)
 
     def get(self, id):
         return Project(self._c, json=self.get_properties(id))
